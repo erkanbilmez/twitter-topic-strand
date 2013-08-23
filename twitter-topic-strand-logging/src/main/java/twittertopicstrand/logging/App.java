8537324431d12b3dc7919ca49a08ef6eb0151c95
celebisait <celebisait@gmail.com>
2013-08-23 12:35:27 -0400

    analyzing ..

--- twitter-topic-strand-logging/src/main/java/twittertopicstrand/logging/App.java
+++ twitter-topic-strand-logging/src/main/java/twittertopicstrand/logging/App.java
@@ -1,40 +1,8 @@
 package twittertopicstrand.logging;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
 import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.commons.lang.ArrayUtils;
-import org.joda.time.DateTime;
-import org.joda.time.Duration;
-import org.json.simple.JSONObject;
 import org.xml.sax.SAXException;
-
-import twitter4j.LightStatus;
-import twitter4j.Status;
-import twitter4j.StatusFactory;
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.User;
-import twitter4j.UserFactory;
-import twitter4j.UserMentionEntity;
-import twitter4j.internal.json.StatusJSONImpl;
-import twitter4j.internal.json.UserJSONImpl;
-import twitter4j.internal.org.json.JSONException;
-import twittertopicstrand.util.FileOperations;
-import twittertopicstrand.util.HourOperations;
-import twittertopicstrand.util.MailSender;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 /**
  * Hello world!
